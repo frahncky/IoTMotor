26ab2ccd0ce4cd7adb565145e24f9209a0532cf3
frahncky <francisco.dsantos.viana@gmail.com>
2026-03-07 18:10:42 -0300

    Corrige travamento no splash ao reabrir app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/widgets.dart';
+﻿import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'app/iot_motor_app.dart';
-import 'features/iot_motor/services/background_mqtt_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GoogleFonts.config.allowRuntimeFetching = true;
-  await BackgroundMqttService.instance.initialize();
   runApp(const MotorControlApp());
 }
